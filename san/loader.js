5cdf07a0ed5f06b9de41e07b0da109b774861481
木杉 <zhmushan@qq.com>
2021-08-04 16:09:15 +0800

    .

--- san/loader.js
+++ san/loader.js
@@ -1,7 +1,7 @@
-const MagicString = require('magic-string');
-const { tokenize, constructTree } = require('hyntax');
-const { Project, SyntaxKind } = require('ts-morph');
-const { getOptions } = require('loader-utils');
+const MagicString = require("magic-string");
+const { tokenize, constructTree } = require("hyntax");
+const { Project, SyntaxKind } = require("ts-morph");
+const { getOptions } = require("loader-utils");
 
 module.exports = async function (source) {
   const magicContent = new MagicString(source);
@@ -9,9 +9,12 @@ module.exports = async function (source) {
   const { ast } = constructTree(tokens);
 
   const tpl = ast.content.children.find(
-    ({ nodeType, content }) => nodeType === 'tag' && content.name === 'template'
+    ({ nodeType, content }) =>
+      nodeType === "tag" && content.name === "template",
   );
-  const script = ast.content.children.find(({ nodeType }) => nodeType === 'script');
+  const script = ast.content.children.find(({ nodeType }) =>
+    nodeType === "script"
+  );
 
   let stack = [tpl];
   let usedIndex = 0;
@@ -19,29 +22,35 @@ module.exports = async function (source) {
   for (let curr = tpl.content, i = 0; i < stack.length; ++i) {
     curr = stack[i].content;
     if (curr.children) {
-      stack.push(...curr.children.filter(i => i.nodeType === 'tag'));
+      stack.push(...curr.children.filter((i) => i.nodeType === "tag"));
     }
 
     if (curr.attributes) {
       let needInject = false;
       let classAttrNode = undefined;
       for (const attr of curr.attributes) {
-        if (attr.key.content.startsWith('ac-')) {
+        if (attr.key.content.startsWith("ac-")) {
           needInject = true;
-          magicContent.overwrite(attr.key.startPosition, attr.endWrapper.endPosition + 1, '');
+          magicContent.overwrite(
+            attr.key.startPosition,
+            attr.endWrapper.endPosition + 1,
+            "",
+          );
 
           if (!sheetRegistries[usedIndex]) {
             sheetRegistries[usedIndex] = {};
           }
-          sheetRegistries[usedIndex][attr.key.content.slice(3)] = attr.value.content;
-        } else if (attr.key.content === 'class') {
+          sheetRegistries[usedIndex][attr.key.content.slice(3)] =
+            attr.value.content;
+        } else if (attr.key.content === "class") {
           classAttrNode = attr;
         }
       }
       if (needInject) {
         let startPos = curr.openStart.endPosition;
         let endPos = -1;
-        const cls = `class="${classAttrNode?.value.content ?? ''} {{ __θac${usedIndex} }}"`;
+        const cls = `class="${classAttrNode?.value.content ??
+          ""} {{ __θac${usedIndex} }}"`;
         if (classAttrNode) {
           startPos = classAttrNode.key.startPosition;
           endPos = classAttrNode.endWrapper.endPosition;
@@ -62,28 +71,48 @@ module.exports = async function (source) {
   if (script) {
     const basePos = script.content.openEnd.endPosition;
 
-    magicContent.appendRight(basePos + 1, `\r\n${sheetRegistries.map((iter, index) => `const __θac${index}Registry=${JSON.stringify(iter)};`).join('\r\n')}`);
+    magicContent.appendRight(
+      basePos + 1,
+      `\r\n${
+        sheetRegistries.map((iter, index) =>
+          `const __θac${index}Registry=${JSON.stringify(iter)};`
+        ).join("\r\n")
+      }`,
+    );
 
     const proj = new Project();
-    const sourceFile = proj.createSourceFile('temp.ts', script.content.value.content);
-    const exportAssignment = sourceFile.getExportAssignment(i => !i.isExportEquals());
-    const returnStmt = exportAssignment.getDescendantStatements().find(i => i.asKind(SyntaxKind.ReturnStatement));
+    const sourceFile = proj.createSourceFile(
+      "temp.ts",
+      script.content.value.content,
+    );
+    const exportAssignment = sourceFile.getExportAssignment((i) =>
+      !i.isExportEquals()
+    );
+    const returnStmt = exportAssignment.getDescendantStatements().find((i) =>
+      i.asKind(SyntaxKind.ReturnStatement)
+    );
 
     let startPos = basePos + returnStmt.getStart() + 8;
     let endPos = basePos + returnStmt.getEnd() + 1;
-    magicContent.appendRight(startPos, 'Object.assign(');
-    magicContent.appendRight(endPos, `, { ${Array(usedIndex).fill().map((_, i) => `__θac${i}: ''`).join(',')} });`);
-
+    magicContent.appendRight(startPos, "Object.assign(");
+    magicContent.appendRight(
+      endPos,
+      `, { ${
+        Array(usedIndex).fill().map((_, i) => `__θac${i}: ''`).join(",")
+      } });`,
+    );
 
     // Inject event handlers
     magicContent.appendRight(
       basePos + exportAssignment.getStart() + 18,
-      `${Array(usedIndex).fill().map((_, i) => `
+      `${
+        Array(usedIndex).fill().map((_, i) => `
         __ac${i}Trigger({ detail }) {
           const { states } = detail;
           this.data.set('__θac${i}', states.map(i => __θac${i}Registry[i]).join(' '));
         },
-      `).join('')}`
+      `).join("")
+      }`,
     );
   }
 
@@ -94,4 +123,4 @@ module.exports = async function (source) {
   }
 
   return source;
-}
+};
--- san/src/atomic-class.js
+++ san/src/atomic-class.js
@@ -1,9 +1,9 @@
 export function stateHandler(...states) {
-  const acTrigger = new CustomEvent('actrigger', { detail: { states } });
+  const acTrigger = new CustomEvent("actrigger", { detail: { states } });
   /** @param {Event} ev */
   return function (ev) {
     /** @type {HTMLElement} */
     const el = ev.target;
     el.dispatchEvent(acTrigger);
-  }
+  };
 }
