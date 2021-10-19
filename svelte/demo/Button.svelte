edb0db41314718dfdd7bea9164043332dbf0ffab
qiansc <van3008@163.com>
2021-10-19 21:26:43 +0800

    feat: add tailwind vscode extension support

--- svelte/demo/Button.svelte
+++ svelte/demo/Button.svelte
@@ -8,35 +8,36 @@
 
     export let state = 'default';
 
+    /** keyboard support start*/
     onMount(() => {
         document.addEventListener('keydown', keyboardHandler);
         document.addEventListener('keyup', keyboardHandler);
     });
+    function keyboardHandler(event) {
+        state = keyboard(event, state, keycode);
+    }
+    /** keyboard support end*/
 
     function mouseHandler(event) {
         state = mouse(event, state);
     }
-    function keyboardHandler(event) {
-        state = keyboard(event, state, keycode);
-    }
-    $: iconType = state.indexOf('disabled') == -1 ? 'default' : 'block';
 </script>
 <span
     on:mouseenter={mouseHandler}
     on:mouseleave={mouseHandler}
     on:mousedown={mouseHandler}
     on:mouseup={mouseHandler}
-    class="bbb"
     ac-state={state}
     ac-class="px-4 py-2 rounded-lg text-white font-bold m-2 text-base"
     ac-default="bg-green-600"
     ac-hover="bg-green-500 cursor-pointer"
     ac-active="bg-yellow-500"
-    ac-disabled="bg-gray-500 text-gray-100 cursor-not-allowed">
+    ac-disabled="bg-gray-500 text-gray-100 cursor-not-allowed"
+    >
     <span
         class="material-icons align-bottom"
         ac-id="icon"
-        ac-state={iconType}
+        ac-state={state.indexOf('disabled') == -1 ? 'default' : 'block'}
         ac-default="%icon-done"
         ac-block="%icon-block"
     ></span>
